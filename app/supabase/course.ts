909cf13d83086ada2c70f0df6fd25de6513e39c4
jonathanAdanomad <jshan9078@gmail.com>
2024-11-09 18:00:58 -0500

    course endpoints

--- app/supabase/course.ts
+++ app/supabase/course.ts
@@ -7,3 +7,26 @@ export const upsertCourse = async (agent: Partial<Tables<"courses">>) => {
   const { error } = await supabase.from("courses").upsert(agent).select("*");
   if (error) throw error;
 };
+
+export const fetchCourses = async () => {
+  const { data, error } = await supabase.from("courses").select("*");
+
+  if (error) {
+    throw error;
+  }
+  return data;
+};
+
+export const fetchCourse = async (courseId: string) => {
+  const { data, error } = await supabase
+    .from("courses")
+    .select("*")
+    .eq("id", courseId)
+    .single();
+
+  //if no course is found, return null
+  if (error) {
+    return null;
+  }
+  return data;
+};
